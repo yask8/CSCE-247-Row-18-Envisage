1000ab4fce7206841168c45c84e09e3b8da23adc
Stephonj <stephonj@email.sc.edu>
2024-04-04 17:08:35 -0400

    Worked on Advisor Dashboard

--- application/src/main/java/envisage/AdvisingDashboardController.java
+++ application/src/main/java/envisage/AdvisingDashboardController.java
@@ -1,11 +1,27 @@
 package envisage;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class AdvisingDashboardController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Text TextField3;
+
+    @FXML
+    private Text TextField4;
 
     @FXML
     private Button adviseStudentButton;
@@ -26,31 +42,47 @@ public class AdvisingDashboardController {
     private Button studentLookupButton;
 
     @FXML
+    private Text textField1;
+
+    @FXML
+    private Text textField2;
+
+    @FXML
+    private Text textField5;
+
+    @FXML
+    private Text textField6;
+
+    @FXML
     private Button viewMajorMapsButton;
 
     @FXML
-    void adviseStudent(ActionEvent event) {
+    void adviseStudent(MouseEvent event) {
 
     }
 
     @FXML
-    void lookupStudent(ActionEvent event) {
+    void adviseStudent1(MouseEvent event) {
 
     }
 
     @FXML
-    void viewAdvisingProfile(ActionEvent event) {
+    void lookupStudent(MouseEvent event) {
 
     }
 
     @FXML
-    void viewListOfAdvisees(ActionEvent event) {
+    void viewAdvisingProfile(MouseEvent event) {
 
     }
 
     @FXML
-    void viewMajorMaps(ActionEvent event) {
+    void viewListOfAdvisees(MouseEvent event) {
 
     }
 
+    @FXML
+    void viewMajorMaps(MouseEvent event) {
+
+    }
 }
