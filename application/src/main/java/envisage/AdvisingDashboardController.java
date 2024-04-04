130eb4c83612884838b1f7bebf71f2f2b647af74
Stephonj <stephonj@email.sc.edu>
2024-04-04 00:24:56 -0400

    Worked on Advisor Dashboard

---
+++ application/src/main/java/envisage/AdvisingDashboardController.java
@@ -0,0 +1,35 @@
+package envisage;
+import javafx.fxml.FXML;
+
+public class AdvisingDashboardController {
+
+    @FXML
+    private void viewAdvisingProfile() {
+        // Add action for viewing advising profile
+    }
+
+    @FXML
+    private void adviseStudent() {
+        // Add action for advising a student
+    }
+
+    @FXML
+    private void viewMajorMaps() {
+        // Add action for viewing major maps
+    }
+
+    @FXML
+    private void viewListOfAdvisees() {
+        // Add action for viewing list of advisees
+    }
+
+    @FXML
+    private void lookupStudent() {
+        // Add action for looking up a student
+    }
+
+    @FXML
+    private void addOrRemoveAdvisee() {
+        // Add action for adding or removing an advisee
+    }
+}
