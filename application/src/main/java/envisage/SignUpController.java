a0e230c4498907e48a85d3f710444d7cddfadfda
garrettspillman1 <garrettspillman1@gmail.com>
2024-04-06 16:11:47 -0400

    all user dashboardeds are made and correctly get things passed to them

--- application/src/main/java/envisage/SignUpController.java
+++ application/src/main/java/envisage/SignUpController.java
@@ -7,17 +7,14 @@ package envisage;
 import java.io.IOException;
 
 import AdvisingSoftware.*;
-import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.PasswordField;
