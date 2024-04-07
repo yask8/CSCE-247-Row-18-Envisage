d2328d67a7e940c8c3bfa21969215212ffa2326f
unknown <liaz@email.sc.edu>
2024-04-07 05:37:00 -0400

    fixing methods to StudentLookupController

--- application/src/main/java/envisage/AdminDashboardController.java
+++ application/src/main/java/envisage/AdminDashboardController.java
@@ -1,10 +1,9 @@
 package envisage;
 
+import AdvisingSoftware.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import AdvisingSoftware.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,50 +13,55 @@ import javafx.scene.layout.AnchorPane;
 
 public class AdminDashboardController implements Initializable {
 
-    private Facade facade;
-    private User user;
+  private Facade facade;
+  private User user;
 
+  @FXML
+  private Label welcomeLabel;
 
-    @FXML
-    private Label welcomeLabel;
+  @FXML
+  private AnchorPane adminDashboard;
 
-    @FXML
-    private AnchorPane adminDashboard;
+  @FXML
+  private Button adminProfileButton;
 
-    @FXML
-    private Button adminProfileButton;
+  @FXML
+  private Button manageAppAreasButton;
 
-    @FXML
-    private Button manageAppAreasButton;
+  @FXML
+  private Button manageCoursesButton;
 
-    @FXML
-    private Button manageCoursesButton;
+  @FXML
+  private Button manageMajorMapsButton;
 
-    @FXML
-    private Button manageMajorMapsButton;
+  @FXML
+  private Button manageUserButton;
 
-    @FXML
-    private Button manageUserButton;
+  @FXML
+  private Button signOutButton;
 
-    @FXML
-    private Button signOutButton;
+  @FXML
+  private Button studentLookupButton;
 
-    @FXML
-    private Button studentLookupButton;
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    facade = Facade.getInstance();
+    user = facade.getUser();
+    welcomeLabel.setText("Welcome " + user.getFirstName());
+  }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        facade = Facade.getInstance();
-        user = facade.getUser();
-        welcomeLabel.setText("Welcome " + user.getFirstName());
+  @FXML
+  void setStageStudentLookup(ActionEvent event) throws IOException {
+    App.setRoot("studentLookup");
+  }
+
+  @FXML
+  void signOut(ActionEvent event) {
+    try {
+      Facade.getInstance().signOut();
+      App.setRoot("LogIn");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    @FXML
-    void signOut(ActionEvent event) {
-        try {
-            Facade.getInstance().signOut();
-            App.setRoot("LogIn");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
--- application/src/main/java/envisage/AdvisorDashboardController.java
+++ application/src/main/java/envisage/AdvisorDashboardController.java
@@ -1,12 +1,11 @@
 package envisage;
 
+import AdvisingSoftware.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.UUID;
-
-import AdvisingSoftware.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,74 +17,85 @@ import javafx.scene.control.Label;
 
 public class AdvisorDashboardController implements Initializable {
 
-    private Facade facade;
-    private User user;
+  private Facade facade;
+  private User user;
 
-    @FXML
-    private Button adviseStudentButton;
+  @FXML
+  private Button adviseStudentButton;
 
-    @FXML
-    private Button advisorProfileButton;
+  @FXML
+  private Button advisorProfileButton;
 
-    @FXML
-    private Button editListOfAdviseesButton;
+  @FXML
+  private Button editListOfAdviseesButton;
 
-    @FXML
-    private Button listOfAdviseesButton;
+  @FXML
+  private Button listOfAdviseesButton;
 
-    @FXML
-    private PieChart progressPieChart;
+  @FXML
+  private PieChart progressPieChart;
 
-    @FXML
-    private Label riskOfFailureLabel;
+  @FXML
+  private Label riskOfFailureLabel;
 
-    @FXML
-    private Button studentLookupButton;
+  @FXML
+  private Button studentLookupButton;
 
-    @FXML
-    private Label totalNumberOfAdivseesLabel;
+  @FXML
+  private Label totalNumberOfAdivseesLabel;
 
-    @FXML
-    private Button viewMajorMapsButton;
+  @FXML
+  private Button viewMajorMapsButton;
 
-    @FXML
-    private Label welcomeLabel;
+  @FXML
+  private Label welcomeLabel;
 
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    facade = Facade.getInstance();
+    user = facade.getUser();
+    welcomeLabel.setText("Welcome " + user.getFirstName());
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        facade = Facade.getInstance();
-        user = facade.getUser();
-        welcomeLabel.setText("Welcome " + user.getFirstName());
-    
-        ArrayList<UUID> advisees = facade.getListOfAdvisees();
-        ArrayList<UUID> failingStudents = facade.getListOfFailingStudents();
-    
-        int adviseesCount = advisees != null ? advisees.size() : 0;
-        int riskOfFailAdviseesCount = failingStudents != null ? failingStudents.size() : 0;
-    
-        totalNumberOfAdivseesLabel.setText(adviseesCount > 0 ? String.valueOf(adviseesCount) : "No advisees yet");
-        riskOfFailureLabel.setText(riskOfFailAdviseesCount > 0 ? String.valueOf(riskOfFailAdviseesCount) : "No advisees at risk of failing");
-    
-        if (adviseesCount > 0) {
-            ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-                    new PieChart.Data("Low Risk", adviseesCount - riskOfFailAdviseesCount),
-                    new PieChart.Data("Risk of Fail", riskOfFailAdviseesCount)
-            );
-            progressPieChart.setData(pieChartData);
-        } else {
-            progressPieChart.setVisible(false);
-        }
+    ArrayList<UUID> advisees = facade.getListOfAdvisees();
+    ArrayList<UUID> failingStudents = facade.getListOfFailingStudents();
+
+    int adviseesCount = advisees != null ? advisees.size() : 0;
+    int riskOfFailAdviseesCount = failingStudents != null
+      ? failingStudents.size()
+      : 0;
+
+    totalNumberOfAdivseesLabel.setText(
+      adviseesCount > 0 ? String.valueOf(adviseesCount) : "No advisees yet"
+    );
+    riskOfFailureLabel.setText(
+      riskOfFailAdviseesCount > 0
+        ? String.valueOf(riskOfFailAdviseesCount)
+        : "No advisees at risk of failing"
+    );
+
+    if (adviseesCount > 0) {
+      ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+        new PieChart.Data("Low Risk", adviseesCount - riskOfFailAdviseesCount),
+        new PieChart.Data("Risk of Fail", riskOfFailAdviseesCount)
+      );
+      progressPieChart.setData(pieChartData);
+    } else {
+      progressPieChart.setVisible(false);
     }
+  }
 
-    @FXML
-    void signOut(ActionEvent event) {
-        try {
-            Facade.getInstance().signOut();
-            App.setRoot("LogIn");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  @FXML
+  void setStageStudentLookup(ActionEvent event) throws IOException {
+    App.setRoot("studentLookup");
+  }
+
+  @FXML
+  void signOut(ActionEvent event) {
+    try {
+      Facade.getInstance().signOut();
+      App.setRoot("LogIn");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
+  }
 }
