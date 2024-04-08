f263c041f78c21d432f807cabb890f024ef707eb
garrettspillman1 <garrettspillman1@gmail.com>
2024-04-07 20:28:25 -0400

    fixed forgot password and made it a pop up instead

--- application/src/main/java/envisage/LogInController.java
+++ application/src/main/java/envisage/LogInController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import AdvisingSoftware.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -12,6 +14,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class LogInController {
 
@@ -123,7 +127,17 @@ public class LogInController {
 
     @FXML
     void setStageForgotPassword(MouseEvent event) throws IOException {
-        App.setRoot("forgotPassword");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("forgotPassword.fxml"));
+        AnchorPane root = loader.load();
+        
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Forgot Password");
+        stage.setScene(new Scene(root));
+        stage.showAndWait(); 
+        
+
     }
 
 }
