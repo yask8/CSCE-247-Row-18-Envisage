000f2f7cf6ca2e6c7cd0375af48ecd125c0c2c06
Stephonj <stephonj@email.sc.edu>
2024-04-09 12:14:51 -0400

    Edit Course Screen

--- application/src/main/java/envisage/editCourseController.java
+++ application/src/main/java/envisage/editCourseController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class editCourseController {
@@ -74,6 +75,9 @@ public class editCourseController {
 
     @FXML
     private Button SaveButton;
+
+    @FXML
+    private AnchorPane editCoursePane;
 
     @FXML
     void AssignTo(MouseEvent event) {
