7d22dcb9276b6035155da8f11c5bbc67534cf30c
Stephonj <stephonj@email.sc.edu>
2024-04-09 00:03:53 -0400

    Edit Course Screen

---
+++ application/src/main/java/envisage/editCourseController.java
@@ -0,0 +1,79 @@
+package envisage;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class editCourseController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ChoiceBox<?> AssignToChoiceBox;
+
+    @FXML
+    private Button BackButton;
+
+    @FXML
+    private Text CarolinaCoreLabel;
+
+    @FXML
+    private Text CarolinaCoreText;
+
+    @FXML
+    private Text CourseCodeLabel;
+
+    @FXML
+    private Text CourseCodeText;
+
+    @FXML
+    private Text CourseCreditHourText;
+
+    @FXML
+    private Text CourseDescriptionLabel;
+
+    @FXML
+    private Text CourseDescriptionText;
+
+    @FXML
+    private Text CourseIDLabel;
+
+    @FXML
+    private Text CourseIDText;
+
+    @FXML
+    private Text CourseLabel;
+
+    @FXML
+    private Text CourseText;
+
+    @FXML
+    private Text CreditHoursLabel;
+
+    @FXML
+    private Label EditCourseLabel;
+
+    @FXML
+    private AnchorPane EditCoursePane;
+
+    @FXML
+    private Label EditLabel;
+
+    @FXML
+    private Text PreReqLabel;
+
+    @FXML
+    private Text PreReqTextField;
+
+    @FXML
+    private Button SaveButton;
+
+}
