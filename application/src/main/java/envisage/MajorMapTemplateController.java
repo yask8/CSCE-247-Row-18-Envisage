f8b6159a6afbd853412f21a5a73103d18e9cdfe6
garrettspillman1 <garrettspillman1@gmail.com>
2024-04-17 00:49:52 -0400

    Added majorMap view back in

--- application/src/main/java/envisage/MajorMapTemplateController.java
+++ application/src/main/java/envisage/MajorMapTemplateController.java
@@ -1,9 +1,17 @@
 package envisage;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class MajorMapTemplateController {
 
@@ -15,6 +23,30 @@ public class MajorMapTemplateController {
 
     @FXML
     private Button viewMajorMapButton;
+
+    @FXML
+    void setStageMajorMap(ActionEvent event) {
+        String majorName = majorMapLabel.getText().trim();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MajorMap.fxml"));
+            Parent root = loader.load();
+    
+            MajorMapController controller = loader.getController();
+        
+            if (controller != null) {
+                controller.setMajorName(majorName);
+            } else {
+                System.out.println("MajorMapController is null");
+            }
+    
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void setMajorName(String majorName) {
         majorMapLabel.setText(majorName);
