32e5432419101a769adf081e2c5526e12ddb25b7
garrettspillman1 <garrettspillman1@gmail.com>
2024-04-18 10:07:54 -0400

    adding javadocs

--- application/src/main/java/envisage/AboutController.java
+++ application/src/main/java/envisage/AboutController.java
@@ -7,37 +7,59 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
+/**
+ * Controller class for the About page.
+ * Controls navigation and actions related to the About page UI elements.
+ */
 public class AboutController {
 
+    // FXML elements injected from the corresponding FXML file
     @FXML
-    private AnchorPane About;
+    private AnchorPane About; // AnchorPane containing the About page content
 
     @FXML
-    private Label AboutLabel;
+    private Label AboutLabel; // Label for the About section
 
     @FXML
-    private Label faqLabel;
+    private Label faqLabel; // Label for the FAQ section
 
     @FXML
-    private Label UniversityPartnersLabel;
+    private Label UniversityPartnersLabel; // Label for the University Partners section
 
     @FXML
-    private Label loginLabel;
+    private Label loginLabel; // Label for the Login section
 
+    /**
+     * Switches the scene to the Login page when the loginLabel is clicked.
+     *
+     * @param event Mouse click event on the loginLabel
+     * @throws IOException if an I/O error occurs when setting the root scene
+     */
     @FXML
     void setStageLogin(MouseEvent event) throws IOException {
-        App.setRoot("LogIn");
+        App.setRoot("LogIn"); // Switches the scene to the Login page
     }
 
+    /**
+     * Switches the scene to the University Partners page when the UniversityPartnersLabel is clicked.
+     *
+     * @param event Mouse click event on the UniversityPartnersLabel
+     * @throws IOException if an I/O error occurs when setting the root scene
+     */
     @FXML
-    void setStageUniversityPartners(MouseEvent event) throws IOException{
-        App.setRoot("universityPartners");
+    void setStageUniversityPartners(MouseEvent event) throws IOException {
+        App.setRoot("universityPartners"); // Switches the scene to the University Partners page
     }
 
+    /**
+     * Switches the scene to the FAQ page when the faqLabel is clicked.
+     *
+     * @param event Mouse click event on the faqLabel
+     * @throws IOException if an I/O error occurs when setting the root scene
+     */
     @FXML
-    void setStageFAQ(MouseEvent event) throws IOException{
-        App.setRoot("faqPage");
+    void setStageFAQ(MouseEvent event) throws IOException {
+        App.setRoot("faqPage"); // Switches the scene to the FAQ page
     }
-
 }
 
--- application/src/main/java/envisage/AdminDashboardController.java
+++ application/src/main/java/envisage/AdminDashboardController.java
@@ -9,86 +9,139 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-//Author @Spillmag and Stephon Johnson
-
+/**
+ * Controller class for the Admin Dashboard page.
+ * Handles actions and navigation for the admin dashboard UI.
+ * Authors: @Spillmag and Stephon Johnson
+ */
 public class AdminDashboardController implements Initializable {
 
-  private Facade facade;
-  private User user;
+    // Instance variables
+    private Facade facade;
+    private User user;
 
-  @FXML
-  private Label welcomeLabel;
+    // FXML injected elements
+    @FXML
+    private Label welcomeLabel;
 
-  @FXML
-  private AnchorPane adminDashboard;
+    @FXML
+    private AnchorPane adminDashboard;
 
-  @FXML
-  private Button adminProfileButton;
+    @FXML
+    private Button adminProfileButton;
 
-  @FXML
-  private Button manageAppAreasButton;
+    @FXML
+    private Button manageAppAreasButton;
 
-  @FXML
-  private Button manageCoursesButton;
+    @FXML
+    private Button manageCoursesButton;
 
-  @FXML
-  private Button manageMajorMapsButton;
+    @FXML
+    private Button manageMajorMapsButton;
 
-  @FXML
-  private Button AddOrRemoveUsersButton;
+    @FXML
+    private Button AddOrRemoveUsersButton;
 
-  @FXML
-  private Button signOutButton;
+    @FXML
+    private Button signOutButton;
 
-  @FXML
-  private Button studentLookupButton;
+    @FXML
+    private Button studentLookupButton;
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-    facade = Facade.getInstance();
-    user = facade.getUser();
-    welcomeLabel.setText("Welcome " + user.getFirstName());
-  }
-
-  @FXML
-  void setStageStudentLookup(ActionEvent event) throws IOException {
-    App.setRoot("studentLookup");
-  }
-
-  @FXML
-  void setStageAdvisorProfile(ActionEvent event) throws IOException {
-    App.setRoot("advisorProfile");
-  }
-
-  @FXML
-  void setStageAdminProfile(ActionEvent event) {}
-
-  @FXML
-  void setStageMajorList(ActionEvent event) throws IOException {
-    App.setRoot("majorList");
-  }
-
-  @FXML
-  void setStageAddRemoveUsers(ActionEvent event) throws IOException {
-    App.setRoot("AdminAddOrRemove");
-    
-  }
-
-  @FXML
-  void setStageManageCourses(ActionEvent event) throws IOException {
-    App.setRoot("courseList");
-  }
-
-  @FXML
-  void signOut(ActionEvent event) {
-    try {
-      Facade.getInstance().signOut();
-      App.setRoot("LogIn");
-    } catch (IOException e) {
-      e.printStackTrace();
+    /**
+     * Initializes the controller after its root element has been completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object,
+     *                  or null if the location is not known.
+     * @param resources The resources used to localize the root object, or null
+     *                  if the root object was not localized.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        facade = Facade.getInstance();
+        user = facade.getUser();
+        welcomeLabel.setText("Welcome " + user.getFirstName());
     }
-  }
+
+    /**
+     * Navigates to the Student Lookup page.
+     *
+     * @param event The event representing the action.
+     * @throws IOException if an I/O error occurs when setting the root scene.
+     */
+    @FXML
+    void setStageStudentLookup(ActionEvent event) throws IOException {
+        App.setRoot("studentLookup");
+    }
+
+    /**
+     * Navigates to the Advisor Profile page.
+     *
+     * @param event The event representing the action.
+     * @throws IOException if an I/O error occurs when setting the root scene.
+     */
+    @FXML
+    void setStageAdvisorProfile(ActionEvent event) throws IOException {
+        App.setRoot("advisorProfile");
+    }
+
+    /**
+     * Navigates to the Admin Profile page.
+     *
+     * @param event The event representing the action.
+     */
+    @FXML
+    void setStageAdminProfile(ActionEvent event) {
+        // Implement navigation to Admin Profile page
+    }
+
+    /**
+     * Navigates to the Major List page.
+     *
+     * @param event The event representing the action.
+     * @throws IOException if an I/O error occurs when setting the root scene.
+     */
+    @FXML
+    void setStageMajorList(ActionEvent event) throws IOException {
+        App.setRoot("majorList");
+    }
+
+    /**
+     * Navigates to the Admin Add or Remove Users page.
+     *
+     * @param event The event representing the action.
+     * @throws IOException if an I/O error occurs when setting the root scene.
+     */
+    @FXML
+    void setStageAddRemoveUsers(ActionEvent event) throws IOException {
+        App.setRoot("AdminAddOrRemove");
+    }
+
+    /**
+     * Navigates to the Course List page.
+     *
+     * @param event The event representing the action.
+     * @throws IOException if an I/O error occurs when setting the root scene.
+     */
+    @FXML
+    void setStageManageCourses(ActionEvent event) throws IOException {
+        App.setRoot("courseList");
+    }
+
+    /**
+     * Signs out the user and navigates to the Login page.
+     *
+     * @param event The event representing the action.
+     */
+    @FXML
+    void signOut(ActionEvent event) {
+        try {
+            Facade.getInstance().signOut();
+            App.setRoot("LogIn");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
